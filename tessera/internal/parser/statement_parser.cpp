db516a238b339defa5a20a7f35eaa80aaa5b4086
jwezorek <jwezorek@bluebeam.com>
2020-02-07 16:07:08 -0800

    mo stmt parser

--- tessera/internal/parser/statement_parser.cpp
+++ tessera/internal/parser/statement_parser.cpp
@@ -27,16 +27,26 @@ namespace tess {
         auto make_ = [&](auto& ctx) { _val(ctx) = std::make_shared<T>(_attr(ctx)); };
 
         const auto expr = expression_();
-        const auto obj_ref = object_ref_expr_();
+        const auto obj_ref_generic = object_ref_expr_();
 
+        x3::rule<class obj_ref_, obj_ref_ptr> const obj_ref = "obj_ref";
+        x3::rule<class obj_ref_pair_, std::tuple< obj_ref_ptr, obj_ref_ptr>> obj_ref_pair = "obj_ref_pair";
         x3::rule<class lay_stmt_aux_, lay_params> const lay_stmt_aux = "lay_stmt_aux";
         x3::rule<class lay_stmt_, stmt_ptr> const lay_stmt = "lay_stmt";
         x3::rule<class if_stmt_aux_, if_params> const if_stmt_aux = "if_stmt_aux";
         x3::rule<class if_stmt_, stmt_ptr> const if_stmt = "if_stmt";
 
-        //auto const 
+        auto const obj_ref_def = obj_ref_generic;
+        auto const obj_ref_pair_def = obj_ref >> "<->" >> obj_ref;
+        auto const lay_stmt_aux_def = kw_lit<kw::lay>() >> (obj_ref% x3::lit("->")) >> kw_lit<kw::such_that>() >> (obj_ref_pair% x3::lit(',')) >> x3::lit(';');
 
-        //auto const lay_stmt_aux_def = kw_lit<kw::lay>() >> (obj_ref % x3::lit("->")) >> kw_lit<kw::such_that>() >>
+        BOOST_SPIRIT_DEFINE(
+            obj_ref,
+            obj_ref_pair,
+            lay_stmt_aux
+        );
+
+        
 
     }
 }
